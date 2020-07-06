52ab3f358bc8aa96fc1369dac449381a98d4d4d5
pathum <awpathum@gmail.com>
2020-07-06 20:05:52 +0530

    JSP section done

--- src/main/java/com/linkedin/servlet/CatalogServlet.java
+++ src/main/java/com/linkedin/servlet/CatalogServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.AsyncContext;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.Cookie;
@@ -30,7 +31,7 @@ public class CatalogServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append(request.getParameter("name"));
+		//response.getWriter().append(request.getParameter("name"));
 	}
 
 	/**
@@ -38,55 +39,14 @@ public class CatalogServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		AsyncContext asyncContext = request.startAsync();
-		
-		asyncContext.start(new Runnable() {
-			
-			@Override
-			public void run() {
-				try {
-					Thread.sleep(5000);
-					System.out.println("Print the response");
-					System.out.println("Response returned by : " + Thread.currentThread().getName());
-					returnResponse(request, response);
-					asyncContext.complete();
-				}catch(InterruptedException e) {
-					e.printStackTrace();
-				}catch(IOException e) {
-					e.printStackTrace();
-				}
-				
-			}
-		});
-		System.out.println("Initial Request : " + Thread.currentThread().getName());
-	}
-
-	private void returnResponse(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		String name = request.getParameter("name");
 		String manufacturer = request.getParameter("manufacturer");
 		String sku = request.getParameter("sku");
 		
 		Catalog.addItem(new CatalogItem(name,manufacturer,sku));
 		
-		response.setHeader("someHeader", "someHeaderValue");
-		response.addCookie(new Cookie("someCookie","someCookieValue"));
-		
-		PrintWriter out = response.getWriter();
-		
-		out.println("<html>");
-		out.println("<head></head>");
-		out.println("<body>");
-		out.println("<table>");
-		for(CatalogItem item: Catalog.getItems()) {
-			out.println("<tr>");
-			out.println("<td>");
-			out.println(item.getName());
-			out.println("</td>");
-			out.println("</tr>");
-		}
-		out.println("</body>");
-		out.println("</table>");
-		out.println("</html>");
+		request.setAttribute("items", Catalog.getItems());
+		RequestDispatcher dispatcher = request.getRequestDispatcher("list.jsp");
+		dispatcher.forward(request, response);
 	}
-
 }
