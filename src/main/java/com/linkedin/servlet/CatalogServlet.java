b81da1b32b10b611d9d52ff1fb2c5e2773b09629
pathum <awpathum@gmail.com>
2020-07-06 18:47:33 +0530

    Servlet API done

--- src/main/java/com/linkedin/servlet/CatalogServlet.java
+++ src/main/java/com/linkedin/servlet/CatalogServlet.java
@@ -1,8 +1,12 @@
 package com.linkedin.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.AsyncContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,7 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class CatalogServlet
  */
-@WebServlet("/CatalogServlet")
+@WebServlet(urlPatterns="/CatalogServlet",asyncSupported=true)
 public class CatalogServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -33,8 +37,56 @@ public class CatalogServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		
+		AsyncContext asyncContext = request.startAsync();
+		
+		asyncContext.start(new Runnable() {
+			
+			@Override
+			public void run() {
+				try {
+					Thread.sleep(5000);
+					System.out.println("Print the response");
+					System.out.println("Response returned by : " + Thread.currentThread().getName());
+					returnResponse(request, response);
+					asyncContext.complete();
+				}catch(InterruptedException e) {
+					e.printStackTrace();
+				}catch(IOException e) {
+					e.printStackTrace();
+				}
+				
+			}
+		});
+		System.out.println("Initial Request : " + Thread.currentThread().getName());
+	}
+
+	private void returnResponse(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		String name = request.getParameter("name");
+		String manufacturer = request.getParameter("manufacturer");
+		String sku = request.getParameter("sku");
+		
+		Catalog.addItem(new CatalogItem(name,manufacturer,sku));
+		
+		response.setHeader("someHeader", "someHeaderValue");
+		response.addCookie(new Cookie("someCookie","someCookieValue"));
+		
+		PrintWriter out = response.getWriter();
+		
+		out.println("<html>");
+		out.println("<head></head>");
+		out.println("<body>");
+		out.println("<table>");
+		for(CatalogItem item: Catalog.getItems()) {
+			out.println("<tr>");
+			out.println("<td>");
+			out.println(item.getName());
+			out.println("</td>");
+			out.println("</tr>");
+		}
+		out.println("</body>");
+		out.println("</table>");
+		out.println("</html>");
 	}
 
 }
